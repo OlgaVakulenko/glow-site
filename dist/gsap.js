aeb96c38f2939d6f4482adbab03ec180aa10fbd9
sergey-bogdan <sergey.bogdan.vi@gmail.com>
2023-03-01 15:00:46 +0300

    temp

--- dist/gsap.js
+++ dist/gsap.js
@@ -2,8 +2,14 @@ import gsap from 'gsap';
 import ScrollTrigger from 'gsap/dist/ScrollTrigger';
 import ScrollSmoother from './ScrollSmoother';
 import ScrollToPlugin from 'gsap/dist/ScrollToPlugin';
+import { isBrowser, isClient } from '../lib/utils';
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother, ScrollToPlugin);
+
+if (isClient && isBrowser('Safari')) {
+  console.log('ScrollTrigger.normalizeScroll');
+  ScrollTrigger.normalizeScroll(true);
+}
 
 export default gsap;
 export { ScrollSmoother, ScrollTrigger };
