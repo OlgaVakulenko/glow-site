d7531a1ebd352cd2898933a3d6205fc2bc504d63
sergey-bogdan <sergey.bogdan.vi@gmail.com>
2023-07-27 13:55:41 +0300

    temp

--- components/Analytics/index.js
+++ components/Analytics/index.js
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import Script from 'next/script';
 import { useEffect } from 'react';
 // import Amplitude from './Amplitude';
 // import FacebookPixel from './FacebookPixel';
@@ -54,7 +55,23 @@ export default function Analytics() {
     <>
       {/* <Amplitude /> */}
       <Hotjar />
-      <GTag ids={['G-5NP2XWNRBX', 'GTM-MW59PP8']} />
+      <GTag ids={['G-5NP2XWNRBX']} />
+      <Script id="ga-2">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MW59PP8');`}
+      </Script>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-MW59PP8"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
+
       {/* <GTag id="GTM-MW59PP8" /> */}
       {/* <FacebookPixel /> */}
       <MixPanel />
