fc250f489e8e29b1218a4cd19d3eb101b75d95f2
sergey-bogdan <sergey.bogdan.vi@gmail.com>
2024-01-16 16:16:28 +0200

    temp

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,66 +1,55 @@
+import { Disclosure, Transition } from '@headlessui/react';
+import cx from 'clsx';
 import FooterFormWrapper from '../components/FooterFormWrapper';
 import Layout from '../components/Layout';
 import DefaultLayout from '../components/Pages/Layouts/DefaultLayout';
-import { Disclosure, Transition } from '@headlessui/react';
-import cx from 'clsx';
 
 const items = [
   {
     q: `What does your company stand for?`,
-    a: `We stand for bringing complex solutions in simple and comprehensible forms. Turn complex
-    digital systems into clean and usable products.
-    We care for consistent user flow from the first demo to full-scale Omni channel operations.
-    We ensure that for the user the first experience pays off and the last never comes.
-    
+    a: `We stand for bringing complex solutions in simple and comprehensible forms. Turn complex digital systems into clean and usable products. We care for consistent user flow from the first demo to full-scale Omni channel operations. We ensure that for the user the first experience pays off and the last never comes.
+
     At Glow, we believe that strong relationships and transparent communication are the foundation for collaboration success.`,
   },
   {
     q: `How much do you charge?`,
-    a: `We provide the estimate after the discovery call with the client (You can book a call right
-      now).
-      Our pricing model is a flat rate per hour. The rate itself depends on the amount of work, time
-      estimate, and number of required specialists. 90% of our projects start with a proper
-      estimation process that result in a clear cost structure and a timeframe.`,
+    a: `We provide the estimate after the discovery call with the client (You can book a call right now).
+
+      Our pricing model is a flat rate per hour. The rate itself depends on the amount of work, time estimate, and number of required specialists. 90% of our projects start with a proper estimation process that result in a clear cost structure and a timeframe.`,
   },
   {
     q: `Team allocation.`,
-    a: `Team size may vary due to the scale of the project. Nevertheless, we always provide our
-    customers with a product designer, a lead designer, and a project manager.`,
+    a: `Team size may vary due to the scale of the project. Nevertheless, we always provide our customers with a product designer, a lead designer, and a project manager.`,
   },
   {
     q: `How does our cooperation start?`,
-    a: `We start with an intro call. After the call and review of all shared materials, we create a
-    project estimation with determined time range, cost, and team size.`,
+    a: `We start with an intro call. After the call and review of all shared materials, we create a project estimation with determined time range, cost, and team size.`,
   },
   {
     q: `What about edits and iterations revision?`,
-    a: `Our expertise and design process helps us minimize the number of revisions. To achieve
-    this we conduct thorough discovery sessions with the lead designer and project manager to
-    define the design and general product direction for future updates.
-    We also bring several test-ready versions for each solution so the best one can be
-    determined.
-    There are cases where we suggest new features or the scope of work results to be bigger
-    than was expected. In this case, we take additional time to agree on new features or to
-    adjust the work scope affecting the roadmap and pushing deadlines.`,
+    a: `Our expertise and design process helps us minimize the number of revisions. To achieve this we conduct thorough discovery sessions with the lead designer and project manager to define the design and general product direction for future updates.
+    
+    We also bring several test-ready versions for each solution so the best one can be determined.
+    
+    There are cases where we suggest new features or the scope of work results to be bigger than was expected. In this case, we take additional time to agree on new features or to adjust the work scope affecting the roadmap and pushing deadlines.`,
   },
   {
     q: `What tools do you use?`,
-    a: `Figma Jam – for research, generating ideas, and workshops with clients.
-    ClickUp – our project management tool to coordinate within the team and meet deadlines.
+    a: `Figma Jam - for research, generating ideas, and workshops with clients.
+    ClickUp - our project management tool to coordinate within the team and meet deadlines.
     
-    Figma – our main design tool.
-    Slack – client & agency communication (Each project gets a separate channel).
-    E-mail – formal communication for updates and mockup approvals.`,
+    Figma - our main design tool.
+    Slack - client & agency communication (Each project gets a separate channel).
+    E-mail - formal communication for updates and mockup approvals.`,
   },
   {
     q: `Do you provide only the design for my project, or do the development as well?`,
-    a: `We do not provide development but we do every other thing related to product design. Also,
-    we always keep in touch with developers and make sure that the communication is friendly
-    and transparent.`,
+    a: `We do not provide development but we do every other thing related to product design. Also, we always keep in touch with developers and make sure that the communication is friendly and transparent.`,
   },
   {
     q: `Do you guys work only with big companies? We’re an early-stage startup.`,
     a: `We work with different kinds of projects and are happy to help your startup get its spark.
+    
     Contact us and we will discuss details 😊`,
   },
   {
@@ -81,7 +70,7 @@ function FaqItem({ question, answer }) {
               </div>
               <div className="flex justify-between text-body-s md:col-span-5 xl:col-span-7">
                 {open ? (
-                  <Disclosure.Panel className="hidden md:block">
+                  <Disclosure.Panel className="hidden whitespace-pre-line md:block">
                     {answer}
                   </Disclosure.Panel>
                 ) : (
@@ -131,7 +120,7 @@ function FaqItem({ question, answer }) {
             leaveFrom="transform scale-100 opacity-100"
             leaveTo="transform scale-95 opacity-0"
           >
-            <Disclosure.Panel className="mt-8 text-body-s">
+            <Disclosure.Panel className="mt-8 whitespace-pre-line text-body-s ">
               {answer}
             </Disclosure.Panel>
           </Transition>
