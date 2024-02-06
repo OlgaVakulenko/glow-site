5c78dd74c849f288120c398e61a17856aeef714e
sergey-bogdan <sergey.bogdan.vi@gmail.com>
2024-02-06 12:46:47 +0200

    temp

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://glow.team',
-  generateRobotsTxt: true, // (optional)
-  robotsTxtOptions: {
-    policies: [{ userAgent: '*', allow: '/', disallow: '/form-success' }],
-  },
+  generateRobotsTxt: false, // (optional)
+  // robotsTxtOptions: {
+  //   policies: [
+  //     { userAgent: '*', allow: '/', disallow: '/form-success' },
+  //     { userAgent: '*', disallow: '/scene' },
+  //     { userAgent: '*', disallow: '/cgi-bin' },
+  //     { userAgent: '*', disallow: '*?' },
+  //   ],
+  // },
   outDir: 'out',
   // ...other options
 };
