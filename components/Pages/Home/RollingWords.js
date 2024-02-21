26bc8dde46e4d0d817139db6e4d8fb1a9227bbc2
sergey-bogdan <sergey.bogdan.vi@gmail.com>
2024-02-21 13:40:59 +0200

    temp

--- components/Pages/Home/RollingWords.js
+++ components/Pages/Home/RollingWords.js
@@ -125,7 +125,7 @@ export function RollingWords({ words, interval = 16500 }) {
         );
       })}
     </span>
-  )
+  );
 }
 
 function TmpBg({ children }) {
@@ -163,75 +163,6 @@ function TmpBg({ children }) {
   return (
     <div ref={tmpRef} style={{ paddingTop: media !== 'mobile' ? 200 : 0 }}>
       {children}
-    </div>
-  );
-}
-
-export default function Home() {
-  const setHeaderTheme = useSetAtom(headerTheme);
-  const setLogoColor = useSetAtom(logoColor);
-  const changeBgRef = useRef(null);
-  const refScrollContainer = useRef(null);
-
-  // useEffect(() => {
-  //   return;
-  //   const ctx = gsap.context(() => {
-  //     const obj = {
-  //       color: '#19191B',
-  //     };
-  //     gsap.to(obj, {
-  //       scrollTrigger: {
-  //         trigger: firstSectionRef.current,
-  //         start: '100% center',
-  //         end: '130% center',
-  //         scrub: true,
-  //         onUpdate: () => {
-  //           setLogoColor(obj.color);
-  //           console.log(obj.color);
-  //         },
-  //         onEnterBack: () => {
-  //           setHeaderTheme((c) => c.filter((v) => v !== 'white'));
-  //         },
-  //         onLeave: () => {
-  //           setHeaderTheme((c) => [...c, 'white']);
-  //         },
-  //       },
-  //       color: '#E33230',
-  //     });
-  //   });
-
-  //   return () => {
-  //     ctx.revert();
-  //   };
-  // }, []);
-
-  return (
-    <div ref={refScrollContainer}>
-      <IntroSection />
-      <div ref={changeBgRef} className="bg-white">
-        {/** tmp */}
-        {/* <TmpBg> */}
-        {/* <div>
-          <Showreel />
-        </div> */}
-        <Niches />
-        {/* <CasesSlider2 /> */}
-        <CasesSlider />
-        {/* <div className="mt-10 mb-20 flex justify-center md:mb-48 md:-mt-10">
-          <Button href="/work" theme="white">
-            View all case studies
-          </Button>
-        </div> */}
-        <OutProjectsLink hasBorder={true} className="hidden md:block" />
-        {/* </TmpBg> */}
-        <DimSection className="pb-14">
-          <Workflow />
-          <OurClients />
-          <Animated>
-            <Reviews />
-          </Animated>
-        </DimSection>
-      </div>
     </div>
   );
 }
