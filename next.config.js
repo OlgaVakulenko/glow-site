229b35123f4c2964ab0619002359f2e3193d0eec
dmytro.serdiuk <serdiukdmitriy1@gmail.com>
2024-12-18 22:00:52 +0200

    patch

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     unoptimized: true,
   },
+
   experimental: {
     // optimizeCss: {
     //   fonts: true,
